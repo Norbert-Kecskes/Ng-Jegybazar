daf093c845dd5c516c9270bddb4bc95888e2830a
Norberto-DiCaprio <webfejlesztes.kn@gmail.com>
2019-10-21 12:46:39 +0200

    make routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { BidComponent } from './bid/bid.component';
+import { EventComponent } from './event/event.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventcardComponent } from './eventcard/eventcard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'event', component: EventComponent},
+    {path: 'ticket', component: TicketComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+    static routableComponents = [
+        AboutComponent,
+        BidComponent,
+        EventComponent,
+        EventDetailComponent,
+        EventListComponent,
+        EventcardComponent,
+        HomeComponent,
+        LoginComponent,
+        PageNotFoundComponent,
+        ProfileComponent,
+        ProfileEditComponent,
+        RegistrationComponent,
+        TicketComponent,
+        TicketDetailComponent,
+        TicketListComponent
+    ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,60 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
-import { AboutComponent } from './about/about.component';
-import { BidComponent } from './bid/bid.component';
-import { EventComponent } from './event/event.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventcardComponent } from './eventcard/eventcard.component';
+import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { TicketComponent } from './ticket/ticket.component';
-import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    BidComponent,
-    EventComponent,
-    EventDetailComponent,
-    EventListComponent,
-    EventcardComponent,
-    FooterComponent,
-    HomeComponent,
-    JumbotronComponent,
-    LoginComponent,
-    NavbarComponent,
-    PageNotFoundComponent,
-    ProfileComponent,
-    ProfileEditComponent,
-    RegistrationComponent,
-    TicketComponent,
-    TicketDetailComponent,
-    TicketListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CollapseModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        JumbotronComponent,
+        NavbarComponent,
+        ...AppRoutingModule.routableComponents
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        CollapseModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
